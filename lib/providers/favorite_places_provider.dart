796d185d4dd36f9d9f65988b95d6a1f4636e6c77
Edwin Cardenas <eacardenase@unal.edu.co>
2024-02-19 21:18:02 -0500

    feat: create db and insert row on added place

--- lib/providers/favorite_places_provider.dart
+++ lib/providers/favorite_places_provider.dart
@@ -1,4 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 
 import 'package:my_places/models/place.dart';
 
@@ -6,6 +9,25 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
   void addPlace(Place place) async {
+    final dbPath = await sql.getDatabasesPath();
+    final db = await sql.openDatabase(
+      path.join(dbPath, 'places.db'),
+      onCreate: (db, version) {
+        return db.execute(
+            'CREATE TABLE user_places (id TEXT PRIMARY KEY, title TEXT, image TEXT, latitude REAL, longitude REAL, address TEXT)');
+      },
+      version: 1,
+    );
+
+    db.insert('user_places', {
+      'id': place.id,
+      'title': place.title,
+      'image': place.image.path,
+      'latitude': place.location.latitude,
+      'longitude': place.location.latitude,
+      'address': place.location.address,
+    });
+
     state = [
       place,
       ...state,
